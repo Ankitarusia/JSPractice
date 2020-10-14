3ec62717eb94d0685871795092a9139f72966c02
Ankitarusia <71278966+Ankitarusia@users.noreply.github.com>
2020-10-14 17:05:37 +0530

    Add files via upload

--- CreateMusicPlayer/main.js
+++ CreateMusicPlayer/main.js
@@ -9,57 +9,95 @@ let seek_slider = document.querySelector(".seek_slider");
 let volume_slider = document.querySelector(".volume_slider"); 
 let curr_time = document.querySelector(".current-time"); 
 let total_duration = document.querySelector(".total-duration"); 
+let volumeIcon = document.querySelector(".volume_icon");
 let track_index=0;
 let isPlaying=false;
 let updateTimer;
-
+var dataswitchID;
 let current_track= document.createElement('audio');
 
-let track_list=[{
+
+	let track_list=[
+{
+	name: "Beyhadh", 
+    artist: "Rahul Jain", 
+    image: "img/Beyhadh.jpg", 
+    path: "audio/Beyhadh - Rahul Jain.mp3"
+},
+{
 	name: "Adhi Adhi Raat", 
     artist: "Bilal Saeed", 
-    image: "img/image1.jpg", 
-    path: "Adhi Adhi Raat.mp3"
+    image: "img/ganesh.jpg", 
+    path: "audio/Adhi Adhi Raat.mp3"
 },
 {
 	name: "Ek Villain - Galliyan", 
     artist: "Ankit Tiwari", 
-    image: "img/image2.jpg", 
-    path: "Ek Villain - Galliyan.mp3"
+    image: "img/Galliyan.jpg", 
+    path: "audio/Ek Villain - Galliyan.mp3"
 },
 {
-	name: "Khud Ko Tere", 
-    artist: "Mahalakshmi Iyer", 
-    image: "img/image3.jpg", 
-    path: "Khud Ko Tere.mp3"
+	name: "Teri Baat Aur Hai", 
+    artist: "Stebin Ben", 
+    image: "img/teribaatorhai.jpg", 
+    path: "audio/Teri Baat Aur Hai - Stebin Ben.mp3"
 },
-];
+{
+	name: "BADAN PE SITARE", 
+    artist: "Darshan Raval", 
+    image: "img/badanpesitare.jpg", 
+    path: "audio/Badan Pe Sitare (Darshan Raval).mp3"
+},
+{
+	name: "Humko Tum Mil Gaye", 
+    artist: "Vishal Mishra", 
+    image: "img/humkotummilgaye.jpg", 
+    path: "audio/Humko Tum Mil Gaye - Vishal Mishra.mp3"
+},
+{
+	name: "Jaan Ban Gaye - Khuda Haafiz", 
+    artist: "Asees Kaur", 
+    image: "img/jaanbangaye.jpg", 
+    path: "audio/Jaan Ban Gaye - Khuda Haafiz.mp3"
+},
+{
+	name: "Kaise Hua - Kabir Singh", 
+    artist: "Vishal Mishra", 
+    image: "img/kaisehua.jpg", 
+    path: "audio/Kaise Hua - Kabir Singh.mp3"
+}
+]
+
 
 function loadTrack(track_index){
 	clearInterval(updateTimer);
 	resetValues();
+	volumeIcon.addEventListener("click",fmute);
 	current_track.src=track_list[track_index].path;
 	current_track.load();
 track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
 track_name.textContent= track_list[track_index].name;
 track_artist.textContent= track_list[track_index].artist;
-now_playing.textContent= "Playing " + (track_index+1) + "of" + track_list.length;  
+//now_playing.textContent= "Playing " + (track_index+1) + "of" + track_list.length;  
 
 updateTimer= setInterval(seekUpdate, 1000);
 
 current_track.addEventListener("ended", nextTrack);
-
+volume_slider.addEventListener("mousemove",setVolume);
 randombgImage();
 }
+
 function randombgImage(){
-var images = ['img/image1.jpg',
-      'img/image2.jpg',
-      'img/image3.jpg'];
+var images = ['img/Beyhadh.jpg','img/ganesh.jpg',
+      'img/Galliyan.jpg',
+      'img/teribaatorhai.jpg','img/badanpesitare.jpg','img/humkotummilgaye.jpg','img/jaanbangaye.jpg','img/kaisehua.jpg'];
+      var heading=['MUSIC IS LIFE','MUSIC BRINGS RELIEF','MUSIC IS THE SHORTHAND OF EMOTIONS','MUSIC IS LOVE IN SEARCH OF WORD','WHERE WORDS FAIL, MUSIC SPEAKS','MUSIC IS WHAT FEELINGS SOUND LIKE','MUSIC IS AN OUTBURST OF THE SOUL','LIFE IS A SONG. SING IT!'];
 	
 	for (var i = 0; i < images.length; i++) {
-	if (track_index==i) {
-	 document.getElementById('banner').setAttribute("style", "background-image: url(" +images[i] + ");background-repeat: no-repeat;background-size: cover; background-position:50% 50% ");
-	
+	if (dataswitchID==i || track_index==i) {
+	 document.getElementById('banner').setAttribute("style", "background-image: url(" +images[i] + ");background-position:top;transition:all ease;background-repeat: no-repeat;background-size: cover; ");
+	document.getElementById('h1').innerHTML=heading[i];
+	document.getElementById('h1').setAttribute("style",'');
 	}
 	
 }}
@@ -72,12 +110,20 @@ function randombgColor() {
    var images = ['img/image1.jpg','img/image2.jpg','img/image3.jpg'];
    document.getElementById('banner').setAttribute("style", "background-image: url("dir +images[randomCount] + ");background-repeat: no-repeat;background-size: cover cover");
 }
-function randombgColor()
+ /* function randombgImage()
 {
-let red = Math.floor(Math.random() * 256) + 64; 
-let green = Math.floor(Math.random() * 256) + 64; 
-let blue = Math.floor(Math.random() * 256) + 64; 
-  let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")"; 
+	var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+   document.body.style.background = color;
+}
+let red = Math.floor(Math.random() * 256) + 16; 
+let green = Math.floor(Math.random() * 256) + 16; 
+let blue = Math.floor(Math.random() * 256) + 16; 
+let alpha = Math.floor(Math.random() * 256) + 16; 
+  let bgColor = "rgba(" + red + ", " + green + ", " + blue + ","+alpha+")"; 
   document.body.style.background = bgColor; 
 }
  */
@@ -135,7 +181,18 @@ current_track.currentTime = seekto;
 function setVolume() { 
 current_track.volume = volume_slider.value / 100; 
 } 
+function fmute(){
+	if(current_track.muted){
+		current_track.muted=false;
+		volumeIcon.innerHTML='<i class="fa fa-volume-up"></i>';
+		volume_slider.value=100; }
+	else{
+			current_track.muted=true;
+			volumeIcon.innerHTML='<i class="fa fa-volume-off"></i>';
+			volume_slider.value=0;
+		}
 
+	}
 function seekUpdate() { 
 let seekPosition = 0;  
 if (!isNaN(current_track.duration)) { 
@@ -153,6 +210,10 @@ if (!isNaN(current_track.duration)) {
 	total_duration.textContent = durationMinutes + ":" + durationSeconds; 
 } 
 } 
-
 loadTrack(track_index); 
+$(".album-poster").on('click',function(e){
+	dataswitchID=$(this).attr("data-switch");
+	loadTrack(dataswitchID);
+	playTrack();
+});
 
